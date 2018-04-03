186d9615027139c33fcdaa599065680412416564
Alvin Kang <oneinallin1@gmail.com>
2018-04-02 18:10:42 -0700

    Add color scheme, media query (breakpoint), and reorganize CSS

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,12 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
 $(function() {
   $('#btn-open').on('click', function() {
-  	$('.sidebar').toggleClass('display-none');
+  	$('#sidebar').toggleClass('display-none');
   	$('#btn-open').toggleClass('display-none');
-  	$('#map-container').toggleClass('col-9');
+  	$('#map-container').toggleClass('col');
   });
   $('#btn-close').on('click', function() {
-  	$('.sidebar').toggleClass('display-none');
+  	$('#sidebar').toggleClass('display-none');
   	$('#btn-open').toggleClass('display-none');
-  	$('#map-container').toggleClass('col-9');
+  	$('#map-container').toggleClass('col');
   });
 });
